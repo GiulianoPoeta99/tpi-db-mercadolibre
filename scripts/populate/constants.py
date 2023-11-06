cefe58e5530b2360cd781633a9bffe85e9c3aac0
Giuliano Ignacio Poeta <poetaignacio@gmail.com>
2023-11-06 12:33:29 -0300

    se cambio constante

--- scripts/populate/constants.py
+++ scripts/populate/constants.py
@@ -9,7 +9,7 @@ YES_NO = Fore.GREEN + 's' + Fore.RESET + '/' + Fore.RED + 'n' + Fore.RESET
 # si se quiere cambiar el stylo de las barras de carga se puede leer la documentacion de la libreria
 # https://pypi.org/project/alive-progress/
 STYLE_BAR = 'filling'
-STYLE_SPINNER = 'arrows'
+STYLE_SPINNER = 'triangles'
 
 # checks
 ISSUER_COMPANY = ['Visa', 'MasterCard', 'American Express', 'Maestro']
