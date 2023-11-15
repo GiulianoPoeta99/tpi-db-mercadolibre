badb4cc6648afc75e29b8dae24bc897e0856cd2f
NeichS <nachoagusss1@gmail.com>
2023-11-15 18:30:40 -0300

    se corrigio el faker de la resenia de pedido

--- scripts/populate/fakerData.py
+++ scripts/populate/fakerData.py
@@ -98,7 +98,7 @@ def fakeOrder(cursor) -> str:
         particular = random.choice(list(getUsersNotCorporate(cursor)))
         paymentMethodList = list(getPaymentMethod(cursor, particular))
     paymentMethod = random.choice(paymentMethodList) 
-    review = getReview(cursor)
+    review = random.choice(getReview(cursor))
     return f"'{orderDate}', {paymentMethod}, {particular}, {review} "
 
 def fakeItem(cursor) -> str: #queda un error en esta funcion pendiente de resolver
