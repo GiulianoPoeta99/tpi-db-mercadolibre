19b28f1a62e372b3dc845feebc30bafe3959e3dd
Giuliano Ignacio Poeta <poetaignacio@gmail.com>
2023-11-21 19:44:22 -0300

    se saco print

--- scripts/populate/components/querys.py
+++ scripts/populate/components/querys.py
@@ -66,8 +66,6 @@ def getShippingTypeOrder(cursor: psycopg2.extensions.cursor, orderNumber) -> str
     query = f"SELECT tipo_envio FROM envio WHERE id_envio = {orderNumber}" 
     cursor.execute(query)
     result = cursor.fetchone()
-    print(str(orderNumber)+'\n')
-    print(result)
     return result[0]
 
 def getReview(cursor: psycopg2.extensions.cursor) -> int:
